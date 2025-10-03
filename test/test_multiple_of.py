28bba923786582128d0798da65f8f92598db710d
Alex Shapiro <alex@alexshapiro.xyz>
2025-10-03 12:21:56 -0400

    move parent path import out of individual test files

--- test/test_multiple_of.py
+++ test/test_multiple_of.py
@@ -1,10 +1,3 @@
-import sys
-from pathlib import Path
-
-
-# Add parent directory to path to import main
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
 from main import multiple_of
 
 
--- test/test_swiglu.py
+++ test/test_swiglu.py
@@ -1,11 +1,5 @@
-import sys
-from pathlib import Path
-
 import pytest
 import torch
-
-# Add parent directory to path to import main
-sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from main import ModelConfig, SwiGLU
 
